048b09a76d13364e86120a6d0c349a61c7d9ebeb
Julien Vanier <jvanier@gmail.com>
2022-12-17 12:26:30 -0500

    Add loading state when logging in

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,17 +2,23 @@ import { useState } from 'react';
 import styles from '../../styles/Login.module.css';
 import AppContainer from '../components/AppContainer';
 import { performLogin } from '../slices/login';
-import { useAppDispatch } from '../hooks';
+import { useAppDispatch, useAppSelector } from '../hooks';
 
 export default function Login() {
     const dispatch = useAppDispatch();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const { status, errorMessage } = useAppSelector((state) => state.login);
+
+    const loading = status === 'loading';
+    const hasError = status === 'failed';
 
     function handleClick(e) {
         e.preventDefault(true);
         dispatch(performLogin({ username, password }));
     }
+
+    const busyIcon = <i className="bi-hourglass"></i>;
 
     return (
         <AppContainer>
@@ -23,11 +29,14 @@ export default function Login() {
             <h2 className="text-center">Log in</h2>
             <form className={styles.loginForm}>
                 <input type="email" name="email" className="form-control"
-                placeholder="Particle.io email" value={username} onChange={(e) => setUsername(e.target.value)} />
+                placeholder="Particle.io email" disabled={loading}
+                value={username} onChange={(e) => setUsername(e.target.value)} />
                 <input type="password" name="password" className="form-control"
-                placeholder="Particle.io password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button className="btn btn-warning w-100" onClick={handleClick}>
-                Log In
+                placeholder="Particle.io password" disabled={loading}
+                value={password} onChange={(e) => setPassword(e.target.value)} />
+                {hasError && <div className="error">{errorMessage}</div>}
+                <button className="btn btn-warning w-100" onClick={handleClick} disabled={loading}>
+                {loading ? busyIcon : 'Log In'}
                 </button>
             </form>
             </section>
